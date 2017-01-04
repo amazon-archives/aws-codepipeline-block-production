ffef398b6a61672eb1bad935bbf8d37e2a98a93e
Jitendra Rajput <jrajput@amazon.com>
2017-01-04 13:28:19 -0800

    Use absolute path for commands
    Update instance type for HVM AMI
    Restrict S3 permissions for CodeDeploy instance profile

--- scripts/basic_health_check.sh
+++ scripts/basic_health_check.sh
@@ -2,13 +2,13 @@
 
 for i in `seq 1 10`;
 do
-  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80`
+  HTTP_CODE=`/usr/bin/curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80`
   if [ "$HTTP_CODE" == "200" ]; then
-    echo "Successfully pulled root page."
+    /bin/echo "Successfully pulled root page."
     exit 0;
   fi
-  echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying."
-  sleep 10
+  /bin/echo "Attempt to curl endpoint returned HTTP Code $HTTP_CODE. Backing off and retrying."
+  /bin/sleep 10
 done
-echo "Server did not come up after expected time. Failing."
+/bin/echo "Server did not come up after expected time. Failing."
 exit 1
--- scripts/write_codedeploy_config.sh
+++ scripts/write_codedeploy_config.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-mkdir -p /var/codedeploy/tomcat-sample
+/bin/mkdir -p /var/codedeploy/tomcat-sample
 
-cat <<EOF >/var/codedeploy/tomcat-sample/env.properties
+/bin/cat <<EOF >/var/codedeploy/tomcat-sample/env.properties
 APPLICATION_NAME=$APPLICATION_NAME
 DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME
 DEPLOYMENT_ID=$DEPLOYMENT_ID
